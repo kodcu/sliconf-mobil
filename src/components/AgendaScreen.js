0cbbbfaa2a0240219a7b51d873a87210ced98c72
anil <anillcosar@gmail.com>
2017-07-25 15:28:25 +0300

    Pages updated

--- src/components/AgendaScreen.js
+++ src/components/AgendaScreen.js
@@ -2,35 +2,62 @@
  * Created by Muslum on 25.07.2017.
  */
 import React from 'react';
-import { Container, Header, Left, Body, Right, Button, Icon, Title,Content,Card ,CardItem,Separator,ScrollableTab,Thumbnail,ListItem,List,Text ,Item,Tabs,Tab,Footer, FooterTab} from 'native-base';
-
+import {
+    Container,
+    Header,
+    Left,
+    Body,
+    Right,
+    Button,
+    Icon,
+    Title,
+    Content,
+    Card,
+    CardItem,
+    Separator,
+    ScrollableTab,
+    Thumbnail,
+    ListItem,
+    List,
+    Text,
+    Item,
+    Tabs,
+    Tab,
+    Footer,
+    FooterTab,
+    Spinner,
+    TabHeading
+} from 'native-base';
+import { View, StyleSheet } from 'react-native';
 
 class AgendaScreen extends React.Component {
 
     constructor(props) {
         super(props);
-        state = {
-           data:[]
-        }
+    }
+    state = {
+        data:null,
+
     }
 
     static navigationOptions = {
-        header:null
+        header: null
     };
 
-    async componentDidMount() {
-        await this.getEventList("123");
+    componentDidMount(){
+        this.getEventList("ss");
     }
 
 
+
     getEventList(id) {
-        return fetch('https://jsonblob.com/api/jsonBlob/2bf9abed-70df-11e7-9e0d-7fe9bffc9f1c')
+        return fetch('https://jsonblob.com/api/jsonBlob/5094d5b9-7121-11e7-9e0d-89f4fb1c5f60')
             .then((response) => response.json())
             .then((responseJson) => {
                 this.setState({
-                    data: responseJson.event,
+                    data: responseJson.events,
                 }, function () {
-                    console.log(responseJson)
+                    console.log(responseJson);
                 });
             })
             .catch((error) => {
@@ -38,8 +65,61 @@ class AgendaScreen extends React.Component {
             });
     }
 
+    fonksiyon(data){
+        return(
+            <Content >
+                <Tabs renderTabBar={() => <ScrollableTab />}>
+                    {this.state.data.agenda.map((item, i) => (
+                        <Tab heading={ <TabHeading><Text>{item.date}</Text></TabHeading>}>
+                            {item.list.map((list,i) => (
+                                <Content  >
+                                    <Item onPress={() => this.props.navigation.navigate('Comments')}>
+                                        <View style={{borderWidth:.2}}/>
+                                        <CardItem>
+                                            <Left>
+                                                <Body>
+                                                <Text>{list.speaker}</Text>
+                                                <Text note>{list.room}</Text>
+                                                </Body>
+                                            </Left>
+                                        </CardItem>
+                                        <CardItem cardBody style={{padding:10}}>
+                                            <Text>{list.topic}</Text>
+                                        </CardItem>
+                                        <CardItem>
+                                            <Left>
+                                                <Button transparent >
+                                                    <Icon name="time" style={{color:'#000000'}}/>
+                                                    <Text style={{padding:10,color:'#000000'}}>{list.time}</Text>
+                                                </Button>
+                                            </Left>
+                                            <Body>
+                                            <Button transparent>
+                                                <Icon active name="md-trending-up" />
+                                                <Text style={{padding:10}}>{list.level}</Text>
+                                            </Button>
+                                            </Body>
+                                            <Right >
+                                                <Button transparent >
+                                                    <Icon active name="ios-add-outline" />
+                                                    <Text style={{padding:10}}>KATIL</Text>
+                                                </Button>
+                                            </Right>
+                                        </CardItem>
+                                    </Item>
+                                </Content>
+                            ))}
+
+                        </Tab>
+                    ))}
+                </Tabs>
+
+            </Content>
+        )
+    }
+
     render() {
-        this.state = this.props.navigation.state.param;
+
         return <Container >
             <Header>
                 <Left>
@@ -62,66 +142,19 @@ class AgendaScreen extends React.Component {
                 </Right>
             </Header>
             <Content>
-                <Content >
-                    <Tabs renderTabBar={() => <ScrollableTab />}>
-                        {this.state.data.map((item, i) => (
-                            <Tab heading={ <TabHeading><Text>{item.date}</Text></TabHeading>}>
-                                {item.list.map((list,i) => (
-                                    <Content  >
-                                        <Item onPress={() => this.props.navigation.navigate('Comments')}>
-                                        <View style={{borderWidth:.2}}/>
-                                        <CardItem>
-                                            <Left>
-                                                <Body>
-                                                <Text>{list.speaker}</Text>
-                                                <Text note>{list.room}</Text>
-                                                </Body>
-                                            </Left>
-                                        </CardItem>
-                                        <CardItem cardBody style={{padding:10}}>
-                                           <Text>{list.topic}</Text>
-                                        </CardItem>
-                                        <CardItem>
-                                            <Left>
-                                                <Button transparent >
-                                                    <Icon name="time" style={{color:'#000000'}}/>
-                                                    <Text style={{padding:10,color:'#000000'}}>{list.time}</Text>
-                                                </Button>
-                                            </Left>
-                                            <Body>
-                                            <Button transparent>
-                                                <Icon active name="md-trending-up" />
-                                                <Text style={{padding:10}}>{list.level}</Text>
-                                            </Button>
-                                            </Body>
-                                            <Right >
-                                                <Button transparent >
-                                                    <Icon active name="ios-add-outline" />
-                                                    <Text style={{padding:10}}>KATIL</Text>
-                                                </Button>
-                                            </Right>
-                                        </CardItem>
-                                        </Item>
-                                    </Content>
-                                ))}
+                {this.state.data === null ? <Text>Loading</Text>: this.fonksiyon(this.state.data)}
 
-                            </Tab>
-                        ))}
-                    </Tabs>
-
-
-                </Content>
-                <Footer>
-                    <FooterTab>
-                        <Button vertical active>
-                            <Text>Hepsi</Text>
-                        </Button>
-                        <Button vertical>
-                            <Text>Seçimlerim</Text>
-                        </Button>
-                    </FooterTab>
-                </Footer>
             </Content>
+            <Footer>
+                <FooterTab>
+                    <Button vertical active>
+                        <Text>Hepsi</Text>
+                    </Button>
+                    <Button vertical>
+                        <Text>Seçimlerim</Text>
+                    </Button>
+                </FooterTab>
+            </Footer>
         </Container>
     }
 }
