7414c27622c2fd28538544dc42f9438b25d9ef79
Muslum <sezginmuslum@gmail.com>
2017-08-06 17:54:21 +0300

    main screen update

--- src/reducks/API.js
+++ src/reducks/API.js
@@ -1,6 +1,9 @@
 /**
  * Created by Muslum on 30.07.2017.
  */
+const API = "https://jsonblob.com/api/jsonBlob/"
+export const API_EVENTLIST = API + '5fe3a887-70bb-11e7-9e0d-8d0ef0a54cdd';
+export const API_EVENT = API + 'bfb38235-75f1-11e7-9e0d-0d7acc47c25b';
+export const API_LOGIN = API + '1d28e931-7a1d-11e7-9e0d-9b6c09e3cf27';
+export const API_REGISTER = API + '1d28e931-7a1d-11e7-9e0d-9b6c09e3cf27';
 
-export const API_EVENTLIST = 'https://jsonblob.com/api/jsonBlob/5fe3a887-70bb-11e7-9e0d-8d0ef0a54cdd';
-export const API_EVENT = 'https://jsonblob.com/api/jsonBlob/bfb38235-75f1-11e7-9e0d-0d7acc47c25b';
--- src/reducks/module/auth.js
+++ src/reducks/module/auth.js
@@ -1,52 +1,114 @@
 /**
  * Created by Muslum on 29.07.2017.
  */
-const LOAD = 'auth/LOAD';
-const LOAD_SUCCESS = 'auth/LOAD_SUCCESS';
-const LOAD_FAIL = 'auth/LOAD_FAIL';
-const LOGIN = 'auth/LOGIN';
-const LOGIN_SUCCESS = 'auth/LOGIN_SUCCESS';
-const LOGIN_FAIL = 'auth/LOGIN_FAIL';
-const LOGOUT = 'auth/LOGOUT';
-const LOGOUT_SUCCESS = 'auth/LOGOUT_SUCCESS';
-const LOGOUT_FAIL = 'auth/LOGOUT_FAIL';
+
+const types = {
+    LOGIN_REQUEST: 'LOGIN_REQUEST',
+    LOGIN_RESPONSE_SUC: 'LOGIN_RESPONSE_SUC',
+    LOGIN_RESPONSE_FAIL: 'LOGIN_RESPONSE_FAIL',
+    LOGOUT_REQUEST: 'LOGOUT_REQUEST',
+    LOGOUT_RESPONSE: 'LOGOUT_RESPONSE'
+}
+
+import {API_REGISTER, API_LOGIN} from '../API'
 
 const initialState = {
-    loaded: false
+    loading: false,
+    error: false,
+    login: false,
+    user: Object,
+    loginError: String,
+
 };
 
-export default function reducer(state = initialState, action = {}) {
-    switch (action.type) {
-        case LOGIN:
+export const reducer = (state = initialState, action) => {
+    const {type, payload} = action
+
+    switch (type) {
+        case types.LOGIN_REQUEST: {
             return {
                 ...state,
-                loggingIn: true
-            };
-        case LOGIN_SUCCESS:
+                loading: true,
+                error: false,
+                login: false,
+                user: {},
+            }
+        }
+        case types.LOGIN_RESPONSE_SUC: {
             return {
                 ...state,
-                loggingIn: true,
-                user: action
-            };
-        case LOGIN_FAIL:
+                loading: false,
+                error: false,
+                login: true,
+                user: payload,
+            }
+        }
+        case types.LOGIN_RESPONSE_FAIL: {
             return {
                 ...state,
-                loggingIn: false,
-                user: null,
-                loginError: action.error
-            };
-        default:
-            return state;
+                loading: false,
+                error: true,
+                login: false,
+                user: {},
+                errorMessage: payload
+            }
+        }
+        case types.LOGOUT_REQUEST: {
+            return {
+                ...state,
+                loading: false,
+                error: false,
+                login: false,
+                user: {},
+            }
+        }
     }
+    return state;
+
 }
 
-export function loginSuccess(user){
-    return {type: LOGIN_SUCCESS, action:user};
+export const actionCreators = {
+
+    login: ({username, password}) => async (dispatch, getState) => {
+
+        dispatch({
+            type: types.LOGIN_REQUEST
+        })
+
+        if (username === "mslm@kodcu.com" && password === "admin123") {
+            try {
+                const response = await fetch(API_LOGIN)
+                const posts = await response.json()
+
+                dispatch({
+                    type: types.LOGIN_RESPONSE_SUC,
+                    payload: posts.user
+                })
+
+            } catch (e) {
+                dispatch({
+                    type: types.LOGIN_RESPONSE_FAIL,
+                    payload: 'İşleminiz gerçekleştirilemiyor!'
+                })
+            }
+        }else
+            dispatch({
+                type: types.LOGIN_RESPONSE_FAIL,
+                payload: 'Girdiğiniz bilgiler hatali veya eksik'
+            })
+
+
+
+    },
+    logout: () => async (dispatch, getState) => {
+        dispatch({
+            type: types.LOGOUT_REQUEST
+        })
+    }
+
 }
 
-export function loginFailed(){
-    return {type: LOGIN_FAIL};
-}
+export default reducer
 
 
 
