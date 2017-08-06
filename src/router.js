01f22070ad477af2b72fb0c956c6298c6b717c24
Muslum <sezginmuslum@gmail.com>
2017-08-06 17:54:21 +0300

    main screen update

--- src/router.js
+++ src/router.js
@@ -2,16 +2,13 @@ import {StackNavigator} from 'react-navigation';
 import SplashScreen from './container/SplashScreen';
 import MainScreen from './container/MainScreen';
 import HomeScreen from './container/HomeScreen';
-import ReduxScreen from './components/ReduxScreen'
 
 export let SPLASH = 'screen/Splash'
 export let MAIN = 'screen/Main'
 export let HOME = 'screen/Home'
-export let REDUX = 'redux/Hmm'
 
 export default StackNavigator({
     [SPLASH]: {screen: SplashScreen},
     [MAIN]: {screen: MainScreen},
     [HOME]:{screen: HomeScreen},
-    [REDUX]:{screen: ReduxScreen}
 });
