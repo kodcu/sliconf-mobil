67310ce60b70ad7eb2af1c716c2d65db1482af54
anil <anillcosar@gmail.com>
2017-08-30 20:01:50 +0300

    Agenda screen date problem solved

--- src/container/SpeakersScreen.js
+++ src/container/SpeakersScreen.js
@@ -3,39 +3,53 @@ import { View, Text, StyleSheet,ScrollView,Image,TouchableOpacity ,FlatList,Dime
 import { Container, Button, Icon, Title ,Content,CardItem,Thumbnail,Card,List,Item,Footer,FooterTab,Picker, Form, Item as FormItem} from 'native-base';
 import AlphabeticView from '../component/AlphabeticView'
 import Header from "../component/Header";
-DATAS =[
-    {name: "Anıl Coşar",workingat: "Gömsis",about: "dfdfdf",topic: ["A106","A107"]},
-    {name: "Apeaker 1",workingat: "Company 1",about: "dfdfdf",topic: ["A106","A107"]},
-    {name: "Bpeaker 2",workingat: "Company 1",about: "dfdfdf",topic: ["A106","A107"]},
-    {name: "Cpeaker 3",workingat: "Company 1",about: "dfdfdf",topic: ["A106","A107"]},
-    {name: "Dpeaker 4",workingat: "Company 1",about: "dfdfdf",topic: ["A106","A107"]},
-    {name: "Epeaker 5",workingat: "Company 1",about: "dfdfdf",topic: ["A106","A107"]},
-    {name: "Fpeaker 6",workingat: "Company 1",about: "dfdfdf",topic: ["A106","A107"]},
-    {name: "Gpeaker 7",workingat: "Company 1",about: "dfdfdf",topic: ["A106","A107"]},
-    {name: "Göksel Pirnal",workingat: "Big Apps",about: "dfdfdf",topic: ["A103"]},
-    {name: "Hakan Özler",workingat: "Kodsis",about: "dfdfdf",topic: ["A101"]},
-    {name: "Hpeaker 8",workingat: "Company 1",about: "dfdfdf",topic: ["A106","A107"]},
-    {name: "Hüseyin Akdoğan",workingat: "Datasist",about: "dfdfdf",topic: ["A102"]},
-    {name: "Lemi Orhan",workingat: "iyzico",about: "dfdfdf",topic: ["A100"]},
-    {name: "Müslüm Sezgin",workingat: "Gömsis",about: "dfdfdf",topic: ["A105"]},
-    {name: "Talip Teyfur",workingat: "Kodsis",about: "dfdfdf",topic: ["A104"]},
-]
+import {connect} from 'react-redux'
+
+
+
+
 DATAS2=["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","R","S","T","U","V","Y","Z","W"]
-export default class SpeakersScreen extends Component {
+
+const mapStateToProps = (state) => ({
+
+})
+
+class SpeakersScreen extends Component {
 
     static navigationOptions = {
         header: null
     };
 
-    state={
-        speakers:[],
-    }
 
-    speakersList(speakers) {
+
+    state={
+        DATAS :[
+            {name: "Anıl Coşar",workingat: "Gömsis",about: "dfdfdf",topic: ["A106","A107"]},
+            {name: "Apeaker 1",workingat: "Company 1",about: "dfdfdf",topic: ["A106","A107"]},
+            {name: "Bpeaker 2",workingat: "Company 1",about: "dfdfdf",topic: ["A106","A107"]},
+            {name: "Cpeaker 3",workingat: "Company 1",about: "dfdfdf",topic: ["A106","A107"]},
+            {name: "Dpeaker 4",workingat: "Company 1",about: "dfdfdf",topic: ["A106","A107"]},
+            {name: "Epeaker 5",workingat: "Company 1",about: "dfdfdf",topic: ["A106","A107"]},
+            {name: "Fpeaker 6",workingat: "Company 1",about: "dfdfdf",topic: ["A106","A107"]},
+            {name: "Gpeaker 7",workingat: "Company 1",about: "dfdfdf",topic: ["A106","A107"]},
+            {name: "Göksel Pirnal",workingat: "Big Apps",about: "dfdfdf",topic: ["A103"]},
+            {name: "Hakan Özler",workingat: "Kodsis",about: "dfdfdf",topic: ["A101"]},
+            {name: "Hpeaker 8",workingat: "Company 1",about: "dfdfdf",topic: ["A106","A107"]},
+            {name: "Hüseyin Akdoğan",workingat: "Datasist",about: "dfdfdf",topic: ["A102"]},
+            {name: "Lemi Orhan",workingat: "iyzico",about: "dfdfdf",topic: ["A100"]},
+            {name: "Müslüm Sezgin",workingat: "Gömsis",about: "dfdfdf",topic: ["A105"]},
+            {name: "Talip Teyfur",workingat: "Kodsis",about: "dfdfdf",topic: ["A104"]},
+            {name: "Talip Teyfur2",workingat: "Kodsis",about: "dfdfdf",topic: ["A104"]},
+            {name: "Talip Teyfur3",workingat: "Kodsis",about: "dfdfdf",topic: ["A104"]},
+        ],
+        weye:[]
+    }
+    speakersList(sp) {
         let changedSpeakersList = [];
         let myMap = new Map();
-
-        speakers.forEach(function (element)  {
+        console.log(sp)
+        sp.forEach(function (element)  {
+            console.log(element)
             let char = element.name.charAt(0).toUpperCase()
             myMap.get(char) ? array = myMap.get(char) : array = []
             array.push(element)
@@ -52,24 +66,33 @@ export default class SpeakersScreen extends Component {
     _keyExtractor = (item, index) => index;
 
     renderRow (info) {
+        console.log(info.index)
         return <View  style={styles.card}>
             <Thumbnail source={require('../../images/hi.png')} large style={{marginBottom:15}}/>
-            <Text style={{fontSize:15,color:'#000'}}>{info.name}</Text>
-            <Text style={{fontSize:12,color:'#BCBEC0'}}>{info.workingat}</Text>
+            <Text style={{fontSize:15,color:'#000'}}>{info.item.name}</Text>
+            <Text style={{fontSize:12,color:'#BCBEC0'}}>{info.item.workingat}</Text>
         </View>
 
 
     }
     componentWillMount(){
         this.setState({
-            speakers:this.speakersList(DATAS)
+            weye:this.speakersList(this.state.DATAS)
         })
-        console.log(this.state.speakers)
+
+        console.log(this.state.weye)
+    }
+    componentWillUpdate(nextProps, nextState){
+        console.log(nextState)
+    }
+    handleScroll = (event) => {
+        console.log(event.nativeEvent.contentOffset.y)
     }
 
-    myfunction(letter){
-        if(this.state.speakers[letter]!==undefined){
-            let item =this.state.speakers[letter][0]
+    myfunction = (letter) =>{
+        let DATAS=this.state.DATAS
+        if(this.state.weye[letter]!==undefined){
+            let item =this.state.weye[letter][0]
             this.myFlatList.scrollToIndex({ animated: true, index:Math.floor(((DATAS.length/2)/DATAS.length)*DATAS.indexOf(item)) });
         }
 
@@ -78,6 +101,7 @@ export default class SpeakersScreen extends Component {
 
     // Hangi harfe basarsa git arrayda o harfin ilk elemanını bul sonra onun indexine flatlistin scrollto özelliği ile git
     render() {
+
         return (
             <Container style={{backgroundColor:'#fff'}}>
                 <Header leftImage='chevron-left' rightImage='bars'
@@ -87,16 +111,17 @@ export default class SpeakersScreen extends Component {
                 <View style={{flexDirection:'row',flex:1}}>
                     <View style={{flexGrow:0.8}}>
                         <FlatList
-                            data={DATAS}
-                            renderItem={({item}) => this.renderRow(item)}
+                            data={this.state.DATAS}
+                            renderItem={(info) => this.renderRow(info)}
                             keyExtractor={this._keyExtractor}
                             numColumns={2}
                             ref={(list) => this.myFlatList = list}
                             getItemLayout={this.getItemLayout}
+                            onScroll={this.handleScroll}
                         /></View>
                     <View style={{justifyContent:'center',alignItems:'center',margin:10,paddingBottom:10,flexGrow:0.2}}>
-                        {Object.keys(this.state.speakers).map((myi,i) =>
-                            <AlphabeticView key={i} item={myi} onClick={this.myfunction.bind(this)}/>
+                        {Object.keys(this.state.weye).map((myi, i) =>
+                            <AlphabeticView key={i} item={myi} onClick={this.myfunction}/>
                         )}
 
                     </View>
@@ -125,3 +150,5 @@ const styles = StyleSheet.create({
         backgroundColor:'#fff',borderWidth:1,borderRadius:15,borderColor:'#F1F2F2',justifyContent:'center',alignItems:'center'
     }
 })
+
+export default connect(mapStateToProps)(SpeakersScreen)
