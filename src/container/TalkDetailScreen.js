ffb8ff4692f66cf51d1421fe91efe3dae93bd922
anil <anillcosar@gmail.com>
2017-09-17 14:03:54 +0300

    Talk Detail created

---
+++ src/container/TalkDetailScreen.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import { View, Text, StyleSheet } from 'react-native';
+
+export default class TalkDetailScreen extends Component {
+    render() {
+        return (
+            <View style={styles.container}>
+                <Text>TalkDetailScreen</Text>
+            </View>
+        );
+    }
+}
+
+const styles = StyleSheet.create({
+    container: {
+
+    },
+});
