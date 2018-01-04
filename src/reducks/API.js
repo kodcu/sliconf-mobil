ee234c6c0895a4c07704b3b24199f9838d03caac
Muslum <sezginmuslum@gmail.com>
2018-01-04 15:45:17 +0300

    bug fixed

--- src/reducks/API.js
+++ src/reducks/API.js
@@ -5,3 +5,4 @@ export const postREGISTER = sliconfAPI + 'users/register';
 export const postFORGOT = sliconfAPI + 'users/password-reset/send/';
 export const postCOMMENT = sliconfAPI + 'events/comment/add-new';
 export const getCOMMENT = sliconfAPI + 'events/comment/list/';
+export const postVOTE = sliconfAPI + 'events/comment/vote/';
