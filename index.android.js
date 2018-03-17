110fe87432e964edd47b1dcc600a9314b4497323
BerkayGungor <berkaygungor.1996@gmail.com>
2018-03-17 13:26:33 +0300

    Schedule base model
    %10 percent

--- index.android.js
+++ index.android.js
@@ -5,7 +5,6 @@ import {AppRegistry} from "react-native";
 import store from './src/reducks';
 import Index from './src';
 
-
 export default class App extends React.Component {
     render() {
         return (
