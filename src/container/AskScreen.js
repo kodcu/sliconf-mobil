57c35c898e58b976b77e9baa34b8851f6723a6a3
BerkayGungor <berkaygungor.1996@gmail.com>
2018-04-07 15:31:24 +0300

    Issue 743 Prototip
    Basic working version.

--- src/container/AskScreen.js
+++ src/container/AskScreen.js
@@ -1,24 +1,29 @@
 import React, { Component } from 'react';
-import { Alert, Modal, Platform, StyleSheet, Text, TextInput, TouchableOpacity, View, ScrollView } from 'react-native';
-import {connect} from "react-redux";
+import { 
+    Alert, 
+    Modal, 
+    Platform, 
+    StyleSheet, 
+    Text, 
+    TextInput, 
+    TouchableOpacity, 
+    View, 
+    ScrollView 
+} from 'react-native';
+import { connect } from "react-redux";
+import { Input, Picker, Item } from "native-base";
+import Icon from 'react-native-vector-icons/Ionicons';
 import Header from "../component/Header";
 import Color from "../theme/Color";
 import TalkComment from "../component/TalkComment";
-import {Input, Picker, Item} from "native-base";
 import * as Scale from "../theme/Scale";
-import {moderateScale} from "../theme/Scale";
 import Font from "../theme/Font";
-import moment from "moment/moment";
-import {actionCreators} from "../reducks/module/comment";
-import {KeyboardAwareScrollView} from "react-native-keyboard-aware-scroll-view";
-import Icon from 'react-native-vector-icons/Ionicons';
-import SendQuestionModal from "../component/SendQuestionModal";
-import If from '../component/If';
+import SendQuestionView from '../component/SendQuestionView';
 
 const mapStateToProps = (state) => ({
     event: state.event.event,
     loading: state.comment.loading,
-    user: state.auth.login ? state.auth.user : state.authDevice.user,
+    user: state.auth.user,
     userDevice: state.authDevice.user,
     login: state.auth.login,
     error: state.comment.error,
@@ -26,108 +31,76 @@ const mapStateToProps = (state) => ({
 });
 
 class AskScreen extends Component {
-
     state = {
         sessionId: "",
-        messageModal: false,
-        pressedButtonId: ""
+        sessionModal: false,
+        isSessionSelected: false
     };
 
-    closeModal = () => {
-        this.setState({messageModal: false})
+    closeSessionModal = () => {
+        this.setState({ sessionModal: false })
     }
 
-    changeSession(session) {
-        this.setState({
-            sessionId: session
-        })
-        console.log(session)
-    }
-
-    /*getPicker(agenda) {
-        if (Platform.OS === "android") {
-            return (<Picker style={{color: Color.black, width: Scale.width - 30, alignSelf: 'center', height: 60}}
-                            placeholder={"Select a Session"}
-                            selectedValue={this.state.sessionId}
-                            onValueChange={(itemValue, itemIndex) => this.changeSession(itemValue)}>
-                <Picker.Item key={-1} label={'Select a Session'} value={""}/>
-                {agenda.filter(talk => talk.level !== -1).map((item, i) =>
-                    <Picker.Item key={i + 1} label={item.topic} value={item.id}/>
-                )}
-            </Picker>);
-        } else {
-            return(<Picker
-                style={{width: Scale.width - 30, alignSelf: 'center', height: 60}}
-                textStyle={{color: Color.black}}
-                iosHeader="Select session"
-                placeholder="Select a Session"
-                mode="dropdown"
-                selectedValue={this.state.sessionId}
-                onValueChange={this.changeSession.bind(this)}>
-                {agenda.filter(talk => talk.level !== -1).map((item, i) =>
-                    <Item label={item.topic} value={item.id} key={item.id}/>
-                )}
-            </Picker>);
-        }
-    }*/
-    /*
-    * Function that creates an array which holds the buttons.
-    * Takes parameter agenda from render() function
-    * Returns buttons
-    */
-    getTalkButtons(agenda){
-        //Array to hold buttons
+    getTopicButtons(agenda){
         var buttons = []; 
-        //Mapping agenda with value talk
         agenda.map((talk) => {
-            //Push elements to button array
             buttons.push(
                 <TouchableOpacity 
                     key={talk.id}
-                    onPress={() => this.setState({ sessionId: talk.id, pressedButtonId: talk.id })}
+                    onPress={() => this.setState({ sessionId: talk.id, isSessionSelected: true, sessionModal: false })}
                 >
-                    <View style={{ 
-                        flex: 1, 
-                        justifyContent: 'center', 
-                        borderWidth: 0.5, 
-                        borderColor: Color.black,
-                        height: Scale.verticalScale(72)
-                        }}
-                    >   
-                        <If con={this.state.pressedButtonId === talk.id}>
-                            <If.Then>
-                                <Text 
-                                style={{
-                                    ...Font.regular,
-                                    textAlign: 'center',
-                                    color: Color.green,
-                                    fontSize: Scale.verticalScale(16),
-                                    padding: 2
-                                }}
-                                >
-                                {talk.topic}
-                                </Text>
-                            </If.Then>
-                            <If.Else>
-                                <Text 
-                                style={{
-                                    ...Font.regular,
-                                    textAlign: 'center',
-                                    color: Color.black,
-                                    fontSize: Scale.verticalScale(16),
-                                    padding: 2
-                                }}
-                                >
-                                {talk.topic}
-                                </Text>
-                            </If.Else>
-                        </If>     
+                    <View style={styles.selectSessionButton}>   
+                        <Text style={styles.selectASessionText}>{talk.topic}</Text>
                     </View>
                 </TouchableOpacity>
             );    
         });
 
         return buttons;
+    }
+
+    getSessionModal(agenda) {
+        return (
+            <Modal
+                animationType={'slide'}
+                transparent={true}
+                visible={this.state.sessionModal}
+                onRequestClose={() => this.closeSessionModal()}>
+                <View style={{flex: 1, backgroundColor: Color.white, marginTop: Platform.OS === 'ios' ? 16 : 0}}>
+                    <TouchableOpacity style={{padding: 4, margin: 8}} onPress={() => this.closeSessionModal()}>
+                        <Icon name={'ios-close-circle'} size={16} color={Color.gray}/>
+                    </TouchableOpacity>
+                    <ScrollView>
+                        {this.getTopicButtons(agenda)}
+                    </ScrollView>
+                </View>
+            </Modal>
+        );
+    }
+
+    getTalkButton(agenda){
+        var button  = (
+            <TouchableOpacity style={styles.selectSessionButton} onPress={() => this.setState({ sessionModal: true })}>
+                <View style={styles.selectSessionButton}>   
+                    <Text style={styles.selectASessionText}>Select a session</Text>
+                </View>
+            </TouchableOpacity>
+        );
+        if (this.state.isSessionSelected === true) {
+            button = agenda.filter(talk => talk.id === this.state.sessionId).map((value) => 
+                    <TouchableOpacity 
+                        key={value.id} 
+                        style={styles.selectSessionButton}
+                        onPress={() => this.setState({ sessionModal: true })}
+                    >
+                        <View style={styles.selectSessionButton} >   
+                            <Text style={styles.selectSessionButtonText} >{value.topic}</Text>
+                        </View>
+                    </TouchableOpacity>
+            ); 
+        }
+
+        return button;
     }
 
     render() {
@@ -141,40 +114,23 @@ class AskScreen extends Component {
                         }}>
                     <Header.Title title="Ask Question"/>
                 </Header>
-
-                {/*this.getPicker(agenda)*/}
                 <View style={{ flex: 1 }}>
-                    <ScrollView style={{ height: Scale.height / 8 }}>
-                        {this.getTalkButtons(agenda)}
-                    </ScrollView>
-                
-                    <TouchableOpacity 
-                        style={styles.buttonContainer}
-                        onPress={() => this.state.sessionId === null || this.state.sessionId === "" ? 
-                            Alert.alert(
-                                'Warning!',
-                                "Please select a Session",
-                                [
-                                    {text: 'OK'}
-                                ],
-                                {cancelable: false}
-                            ) : this.setState({ messageModal: true })
-                        }
-                    >
-                        <Text style={styles.buttonText}>Send Question</Text>
-                    </TouchableOpacity>
-
-                    <View style={{flex: 1, marginTop: 10}}>
-                        {this.state.sessionId.trim() ? 
+                    <View style={{ flex: 1 }}>
+                        {this.getTalkButton(agenda)}
+                    </View>
+                    {
+                        this.state.isSessionSelected === true ? 
+                        <SendQuestionView sessionId={this.state.sessionId} /> 
+                        : null
+                    }
+                    <View style={{ flex: 1, marginTop: 10 }}>
+                        {this.state.sessionId ? 
                             <TalkComment session={this.state.sessionId} lite={true}/> 
                             : null
                         }
                     </View>
                 </View>
-                <SendQuestionModal sessionId={this.state.sessionId}
-                                   closeModal={this.closeModal}
-                                   visible={this.state.messageModal}/>
-
+                {this.getSessionModal(agenda)}
             </View>
         );
     }
@@ -197,15 +153,27 @@ const styles = StyleSheet.create({
         ...Font.medium,
         textAlign: 'center',
         color: Color.white,
-        fontSize:moderateScale(15)
+        fontSize:Scale.moderateScale(15)
     },
-    topic: {
+    selectSessionButton: {
+        justifyContent: 'center', 
+        borderWidth: 0.2, 
+        borderColor: Color.green,
+        height: Scale.verticalScale(72)
+    },
+    selectSessionButtonText: {
         ...Font.regular,
         textAlign: 'center',
         color: Color.green,
-        fontSize: Scale.verticalScale(18),
-        padding: 5,
-        paddingTop: 0
+        fontSize: Scale.verticalScale(16),
+        padding: 2
+    },
+    selectASessionText: {
+        ...Font.regular,
+        textAlign: 'center',
+        color: Color.black,
+        fontSize: Scale.verticalScale(16),
+        padding: 2
     }
 });
 
