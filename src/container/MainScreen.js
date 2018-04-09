99f5a46aa2f912ba0c06465ea51947a08ff889cf
BerkayGungor <berkaygungor.1996@gmail.com>
2018-04-09 20:02:36 +0300

    Version 1.2.3
    ANDROID white screen fix after splash screen.

--- src/container/MainScreen.js
+++ src/container/MainScreen.js
@@ -159,7 +159,7 @@ class MainScreen extends Component {
                                             style={styles.search} 
                                             onPress={() => this._handlePressSearch(Boolean(storeCode) && storeCode !== '' ? storeCode : code)}>
                                             <View style={{flex: 1, flexDirection: 'row'}}>
-                                                <Text style={styles.title1}>{Boolean(storeCode) && storeCode !== '' ? storeCode.toUpperCase() : code.toUpperCase()}</Text>
+                                                <Text style={styles.title1}>{Boolean(storeCode) && storeCode !== '' ? storeCode : code}</Text>
                                                 <Text style={styles.title2}>{Boolean(storeEventName) && storeEventName !== '' ? storeEventName : eventName}</Text>
                                             </View>
                                         </TouchableOpacity>}
